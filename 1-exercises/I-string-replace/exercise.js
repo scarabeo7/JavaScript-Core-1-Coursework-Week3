ff32e322b151345a243c07207adf4d371c8336d8
Chizim Chinuru <garrohat@yahoo.co.uk>
2020-12-22 02:15:03 +0000

    done exercise G to I

--- 1-exercises/I-string-replace/exercise.js
+++ 1-exercises/I-string-replace/exercise.js
@@ -13,7 +13,11 @@
 let story =
   "I like dogs. One day I went to the park and I saw 10 dogs. It was a great day.";
 
-let result = story.replace("", "");
+let result = story
+  .replace(/dogs/gi, "cats")
+  .replace(/day/gi, "night")
+  .replace("10", "100000")
+  .replace("great", "brilliant");
 
 /* EXPECTED OUTPUT */
 
@@ -41,6 +45,6 @@ test(
 
 test(
   "2. The result of the replace is correct",
-  story,
+  result,
   "I like cats. One night I went to the park and I saw 100000 cats. It was a brilliant night."
 );
